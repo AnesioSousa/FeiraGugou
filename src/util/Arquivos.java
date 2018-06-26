fd289b3b6a91c7d7b7f60b7053ec86214785e1f1
Anésio Sousa <anesios98@gmail.com>
2018-06-26 11:47:02 -0300

    Testes e javadoc: Classe Arquivos

--- src/util/Arquivos.java
+++ src/util/Arquivos.java
@@ -6,11 +6,15 @@ import java.util.Arrays;
 import model.Pagina;
 
 /**
- *
- * @author Anésio
+ * Classe que gera objetos que ajudam na manipulação de arquivos.
+ * @author Anésio Sousa
  */
 public class Arquivos{
 
+    /**
+     * Retorna a os arquivos do repositório em forma de ArrayList.
+     * @return lista de arquivos.
+     */
     public ArrayList<File> obterRepositorio(){
         ArrayList<File> arq = new ArrayList<>();
         try{
@@ -27,6 +31,12 @@ public class Arquivos{
         return arq;
     }
     
+    /**
+     * Recebe uma lista vazia de páginas e uma lista de Files com elementos, e copia dados dos Files para objetos Pagina inserindo na lista vazia. 
+     * @param lista lista vazia de páginas.
+     * @param arq lista com elementos de arquivos.
+     * @return lista cheia de páginas com dados retirados da lista de arquivos.
+     */
     public ArrayList<Pagina> passarArqParaPaginas(ArrayList<Pagina> lista, ArrayList<File> arq){
         for (File a : arq) {
             Pagina pagina = new Pagina();
--- test/util/ArquivosTest.java
+++ test/util/ArquivosTest.java
@@ -1,68 +1,57 @@
 package util;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import model.Pagina;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
- * @author anesio
+ * Teste da classe Arquivos.
+ * @author Anésio Sousa
  */
 public class ArquivosTest {
-    
-    public ArquivosTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     /**
-     * Test of obterRepositorio method, of class Arquivos.
+     * Teste do método obterRepositorio, da classe Arquivos.
      */
     @Test
-    public void testObterRepositorio() {
-        System.out.println("obterRepositorio");
-        Arquivos instance = new Arquivos();
-        ArrayList<File> expResult = null;
-        ArrayList<File> result = instance.obterRepositorio();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testObterRepositorio() {   // Esse teste só passa se o repositório existir e tiver itens.
+        try {
+        
+        Arquivos arquivos = new Arquivos();
+        String dirPadrao = new File ("repo").getCanonicalPath();
+        File diretorio = new File(dirPadrao); 
+        assertTrue(diretorio.exists());
+        ArrayList<File> resultado = arquivos.obterRepositorio();
+        assertNotNull(resultado);
+        } catch (IOException ex) {
+        }
     }
 
     /**
-     * Test of passarArqParaPaginas method, of class Arquivos.
+     * Teste do método passarArqParaPaginas, da classe Arquivos.
      */
     @Test
     public void testPassarArqParaPaginas() {
-        System.out.println("passarArqParaPaginas");
-        ArrayList<Pagina> lista = null;
-        ArrayList<File> arq = null;
-        Arquivos instance = new Arquivos();
-        ArrayList<Pagina> expResult = null;
-        ArrayList<Pagina> result = instance.passarArqParaPaginas(lista, arq);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        try {
+            Arquivos arquivos = new Arquivos();
+            ArrayList<Pagina> lista = new ArrayList<>();
+            ArrayList<File> arq = new ArrayList<>();
+            
+            File[] archives;
+            String caminho = new File("").getCanonicalPath();
+            File dir = new File(caminho);
+            archives = dir.listFiles();
+            arq.addAll(Arrays.asList(archives));
+            
+            lista = arquivos.passarArqParaPaginas(lista, arq);
+            
+            assertFalse(lista.isEmpty());
+        } catch (IOException ex) {
+        }
+        
     }
     
 }
