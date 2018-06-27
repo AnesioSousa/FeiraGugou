079af4d8b7505bff7ef117c83a048113d1d2c839
Anésio Sousa <anesios98@gmail.com>
2018-06-27 03:21:54 -0300

    Inicio da interface gráfica! (To animadão com o Swing)

--- src/view/View.java
+++ src/view/View.java
@@ -1,41 +1,22 @@
 package view;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  *
- * @author Anésio
+ * @author Anésio Sousa
  */
-public class View extends Application{
-    /**
-     * Método main da classe View.
-     * Ele é somente responsável por chamar o construtor launch da aplicação javaFx, iniciando assim a aplicação.
-     * @param args 
-     */
+public class View {
     public static void main(String[] args) {
-        launch(args);
-    }
-    /**
-     * Método onde ficam as cenas, layouts, entre outros.
-     * É onde começa a aplicação javaFx.
-     * @param palco palco inicial da aplicação.
-     * @throws Exception 
-     */
-    @Override
-    public void start(Stage palco) throws Exception {
-        StackPane raiz = new StackPane();
-        Label lblMensagem = new Label("Estou aprendendo JavaFX!");
-        raiz.getChildren().add(lblMensagem);
+        JFrame frame = new JFrame("HelloWorld");
+        frame.pack();
+        frame.setSize(600, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        Scene cena  = new Scene(raiz, 600, 500);
-        palco.setTitle("Aprendendo JavaFx!");
-        palco.setScene(cena);
-        palco.show();
+        JLabel label = new JLabel("Hello world!", JLabel.CENTER);
+        frame.getContentPane().add(label);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
-    
-    
 }
